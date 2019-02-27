3410a6cfc60cbbcf6c4d9db490a8a14c8fd1f4b3
Nam Ninja <namninja@gmail.com>
2019-02-26 17:52:10 -0800

    Setting up heroku deployment

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://zero-admin:zero-admin@cluster0-pfvwv.mongodb.net/restaurants-app?retryWrites=true';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb+srv://zero-admin:zero-admin@cluster0-pfvwv.mongodb.net/test-restaurants-app?retryWrites=true';
 exports.PORT = process.env.PORT || 8080;
